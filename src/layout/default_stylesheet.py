30b54738bd15108edb88f38698216a73a73ca73c
Sujal Singh <email.sujalsingh@gmail.com>
2022-02-03 17:44:13 +0530

    NEW: default stylesheet

---
+++ src/layout/default_stylesheet.py
@@ -0,0 +1,12 @@
+BLOCK_ELEMENTS = [
+    "h1", "h2"
+]
+
+STYLESHEET = {
+    "h1": {
+        "margin-top": 20,
+        "margin-right": 20,
+        "margin-bottom": 20,
+        "margin-left": 20,
+    }
+}
